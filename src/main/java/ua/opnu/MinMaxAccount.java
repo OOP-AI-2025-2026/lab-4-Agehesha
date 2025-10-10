60518042740baca498e49abc653c504343321a84
Agehesha <romashugaev99@gmail.com>
2025-10-10 20:38:25 +0300

    Update MinMaxAccount.java

--- src/main/java/ua/opnu/MinMaxAccount.java
+++ src/main/java/ua/opnu/MinMaxAccount.java
@@ -5,51 +5,53 @@ import ua.opnu.java.inheritance.account.Credit;
 import ua.opnu.java.inheritance.account.Debit;
 import ua.opnu.java.inheritance.account.Startup;
 
+
 public class MinMaxAccount extends BankingAccount {
 
-    private int minBalance;
-    private int maxBalance;
+  private int minBalance;
+  private int maxBalance;
 
-    public MinMaxAccount(Startup startup) {
-        super(startup);
-        int b = getBalance();
-        this.minBalance = b;
-        this.maxBalance = b;
+ 
+  public MinMaxAccount(Startup startup) {
+    super(startup);
+    int b = getBalance();
+    this.minBalance = b;
+    this.maxBalance = b;
+  }
+
+
+  @Override
+  public void credit(Credit credit) {
+    super.credit(credit);
+    int b = getBalance();
+    if (b < minBalance) {
+      minBalance = b;
     }
-
-
-    @Override
-    public void credit(Credit credit) {
-        super.credit(credit);
-        int b = getBalance();
-        if (b < minBalance) {
-            minBalance = b;
-        }
-        if (b > maxBalance) {
-            maxBalance = b;
-        }
+    if (b > maxBalance) {
+      maxBalance = b;
     }
+  }
 
 
-    @Override
-    public void debit(Debit debit) {
-        super.debit(debit);
-        int b = getBalance();
-        if (b < minBalance) {
-            minBalance = b;
-        }
-        if (b > maxBalance) {
-            maxBalance = b;
-        }
+  @Override
+  public void debit(Debit debit) {
+    super.debit(debit);
+    int b = getBalance();
+    if (b < minBalance) {
+      minBalance = b;
     }
-
-
-    public int getMin() {
-        return minBalance;
+    if (b > maxBalance) {
+      maxBalance = b;
     }
+  }
 
 
-    public int getMax() {
-        return maxBalance;
-    }
+  public int getMin() {
+    return minBalance;
+  }
+
+
+  public int getMax() {
+    return maxBalance;
+  }
 }
