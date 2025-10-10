1a8f9663df6a2104b34297078355d89468c80a80
Agehesha <romashugaev99@gmail.com>
2025-10-10 20:38:54 +0300

    Update DiscountBill.java

--- src/main/java/ua/opnu/DiscountBill.java
+++ src/main/java/ua/opnu/DiscountBill.java
@@ -1,51 +1,52 @@
 package ua.opnu;
 
+import ua.opnu.java.inheritance.bill.Employee;
 import ua.opnu.java.inheritance.bill.GroceryBill;
 import ua.opnu.java.inheritance.bill.Item;
-import ua.opnu.java.inheritance.bill.Employee;
+
 
 public class DiscountBill extends GroceryBill {
-  private final boolean regularCustomer;
+
+  private final boolean preferred;
   private int discountCount;
   private double discountAmount;
 
-  public DiscountBill(Employee clerk, boolean regularCustomer) {
-    super(clerk);
-    this.regularCustomer = regularCustomer;
+  public DiscountBill(Employee cashier, boolean preferred) {
+    super(cashier);
+    this.preferred = preferred;
   }
 
   @Override
   public void add(Item item) {
     super.add(item);
-    if (regularCustomer) {
-      double d = item.getDiscount();
-      if (d > 0.0) {
-        discountCount++;
-        discountAmount += d;
-      }
+    if (preferred && item.getDiscount() > 0.0) {
+      discountCount++;
+      discountAmount += item.getDiscount();
     }
   }
 
   @Override
   public double getTotal() {
-    double full = super.getTotal();
-    if (!regularCustomer) return full;
-    return full - discountAmount;
+    double base = super.getTotal();
+    return preferred ? base - discountAmount : base;
   }
 
+ 
   public int getDiscountCount() {
-    return regularCustomer ? discountCount : 0;
+    return preferred ? discountCount : 0;
   }
+
 
   public double getDiscountAmount() {
-    return regularCustomer ? discountAmount : 0.0;
+    return preferred ? discountAmount : 0.0;
   }
 
+
   public double getDiscountPercent() {
-    if (!regularCustomer) return 0.0;
-    double full = super.getTotal();
-    if (full <= 0.0) return 0.0;
-    double paid = full - discountAmount;
-    return 100.0 - (paid * 100.0) / full;
+    double base = super.getTotal();
+    if (!preferred || base <= 0.0) {
+      return 0.0;
+    }
+    return (discountAmount / base) * 100.0;
   }
 }
