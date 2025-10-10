00e372b0e96e76523e5fe0d07e8a32c46b717675
Agehesha <romashugaev99@gmail.com>
2025-10-10 18:37:19 +0300

    Update DiscountBill2.java

--- src/main/java/ua/opnu/DiscountBill2.java
+++ src/main/java/ua/opnu/DiscountBill2.java
@@ -1,16 +1,14 @@
 package ua.opnu;
 
-import java.lang.reflect.Method;
+import ua.opnu.java.inheritance.Employee;
+import ua.opnu.java.inheritance.GroceryBill;
+import ua.opnu.java.inheritance.Item;
 
 public class DiscountBill2 {
-
   private final GroceryBill bill;
   private final boolean regularCustomer;
-
   private int discountCount;
   private double discountAmount;
-  private double fullTotal;
-  private double payTotal;
 
   public DiscountBill2(Employee clerk, boolean regularCustomer) {
     this.bill = new GroceryBill(clerk);
@@ -18,37 +16,22 @@ public class DiscountBill2 {
   }
 
   public void add(Item item) {
-    if (item == null) {
-      return;
-    }
     bill.add(item);
-
-    double price = item.getPrice();
-    double disc = regularCustomer ? extractDiscount(item) : 0.0;
-
-    fullTotal += price;
-    payTotal += price - Math.max(0.0, disc);
-
-    if (disc > 0.0) {
-      discountCount++;
-      discountAmount += disc;
-    }
-  }
-
-  private double extractDiscount(Item item) {
-    try {
-      Method m = item.getClass().getMethod("getDiscount");
-      Object v = m.invoke(item);
-      if (v instanceof Number) {
-        double d = ((Number) v).doubleValue();
-        return d < 0 ? 0.0 : d;
+    if (regularCustomer) {
+      double d = item.getDiscount();
+      if (d > 0.0) {
+        discountCount++;
+        discountAmount += d;
       }
-    } catch (Exception ignored) { }
-    return 0.0;
+    }
   }
 
   public double getTotal() {
-    return regularCustomer ? payTotal : fullTotal;
+    double full = bill.getTotal();
+    if (!regularCustomer) {
+      return full;
+    }
+    return full - discountAmount;
   }
 
   public int getDiscountCount() {
@@ -60,17 +43,14 @@ public class DiscountBill2 {
   }
 
   public double getDiscountPercent() {
-    if (!regularCustomer || fullTotal <= 0.0) {
+    if (!regularCustomer) {
       return 0.0;
     }
-    return 100.0 - (getTotal() * 100.0) / fullTotal;
-  }
-
-  public int getCount() {
-    return bill.getCount();
-  }
-
-  public Employee getClerk() {
-    return bill.getClerk();
+    double full = bill.getTotal();
+    if (full <= 0.0) {
+      return 0.0;
+    }
+    double paid = full - discountAmount;
+    return 100.0 - (paid * 100.0) / full;
   }
 }
