41736afaa0318bddc95c8087ebacbc257b373e7b
Agehesha <romashugaev99@gmail.com>
2025-10-10 21:15:31 +0300

    Update MinMaxAccount.java

--- src/main/java/ua/opnu/MinMaxAccount.java
+++ src/main/java/ua/opnu/MinMaxAccount.java
@@ -5,53 +5,43 @@ import ua.opnu.java.inheritance.account.Credit;
 import ua.opnu.java.inheritance.account.Debit;
 import ua.opnu.java.inheritance.account.Startup;
 
-
+/**
+ * Счёт, который отслеживает минимальный и максимальный баланс,
+ * наблюдавшийся после операций.
+ */
 public class MinMaxAccount extends BankingAccount {
+    private int minBalance;
+    private int maxBalance;
 
-  private int minBalance;
-  private int maxBalance;
-
- 
-  public MinMaxAccount(Startup startup) {
-    super(startup);
-    int b = getBalance();
-    this.minBalance = b;
-    this.maxBalance = b;
-  }
-
-
-  @Override
-  public void credit(Credit credit) {
-    super.credit(credit);
-    int b = getBalance();
-    if (b < minBalance) {
-      minBalance = b;
+    /** Удобный конструктор под тесты: стартовый баланс целым числом. */
+    public MinMaxAccount(int startBalance) {
+        this(new Startup(startBalance));
     }
-    if (b > maxBalance) {
-      maxBalance = b;
+
+    /** Базовый конструктор, если тесты/код передают Startup. */
+    public MinMaxAccount(Startup startup) {
+        super(startup);
+        int bal = getBalance();
+        this.minBalance = bal;
+        this.maxBalance = bal;
     }
-  }
 
-
-  @Override
-  public void debit(Debit debit) {
-    super.debit(debit);
-    int b = getBalance();
-    if (b < minBalance) {
-      minBalance = b;
+    /** Перегрузка под тесты: пополнение целым числом. */
+    public void deposit(int amount) {
+        super.deposit(new Credit(amount));
+        int bal = getBalance();
+        if (bal < minBalance) minBalance = bal;
+        if (bal > maxBalance) maxBalance = bal;
     }
-    if (b > maxBalance) {
-      maxBalance = b;
+
+    /** Перегрузка под тесты: списание целым числом. */
+    public void withdraw(int amount) {
+        super.withdraw(new Debit(amount));
+        int bal = getBalance();
+        if (bal < minBalance) minBalance = bal;
+        if (bal > maxBalance) maxBalance = bal;
     }
-  }
 
-
-  public int getMin() {
-    return minBalance;
-  }
-
-
-  public int getMax() {
-    return maxBalance;
-  }
+    public int getMin() { return minBalance; }
+    public int getMax() { return maxBalance; }
 }
