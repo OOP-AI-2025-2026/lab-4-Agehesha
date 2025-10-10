d550eb2a1286c2d481c7100a693e9df941f477ca
Agehesha <romashugaev99@gmail.com>
2025-10-10 18:36:57 +0300

    Update DiscountBill.java

--- src/main/java/ua/opnu/DiscountBill.java
+++ src/main/java/ua/opnu/DiscountBill.java
@@ -1,120 +1,57 @@
+package ua.opnu;
 
-class Item {
-    private String name;
-    private double price;
-    private double discount;
-    
-    public Item(String name, double price, double discount) {
-        this.name = name;
-        this.price = price;
-        this.discount = discount;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public double getPrice() {
-        return price;
-    }
-    
-    public double getDiscount() {
-        return discount;
-    }
-}
+import ua.opnu.java.inheritance.Employee;
+import ua.opnu.java.inheritance.GroceryBill;
+import ua.opnu.java.inheritance.Item;
 
-class Employee {
-    private String name;
-    
-    public Employee(String name) {
-        this.name = name;
-    }
-    
-    public String getName() {
-        return name;
-    }
-}
+public class DiscountBill extends GroceryBill {
+  private final boolean regularCustomer;
+  private int discountCount;
+  private double discountAmount;
 
-class GroceryBill {
-    private Employee clerk;
-    private java.util.ArrayList<Item> items;
-    
-    public GroceryBill(Employee clerk) {
-        this.clerk = clerk;
-        this.items = new java.util.ArrayList<>();
-    }
-    
-    public void add(Item item) {
-        items.add(item);
-    }
-    
-    public double getTotal() {
-        double total = 0.0;
-        for (Item item : items) {
-            total += item.getPrice();
-        }
-        return total;
-    }
-    
-    public Employee getClerk() {
-        return clerk;
-    }
-    
-    public java.util.ArrayList<Item> getItems() {
-        return items;
-    }
-    
-    public void printReceipt() {
-        System.out.println("Items:");
-        for (Item item : items) {
-            System.out.printf("%s: %.2f грн\n", item.getName(), item.getPrice());
-        }
-        System.out.printf("Total: %.2f грн\n", getTotal());
-    }
-}
+  public DiscountBill(Employee clerk, boolean regularCustomer) {
+    super(clerk);
+    this.regularCustomer = regularCustomer;
+  }
 
-class DiscountBill extends GroceryBill {
-    private boolean regularCustomer;
-    private int discountCount;
-    private double totalDiscount;
-    
-    public DiscountBill(Employee clerk, boolean regularCustomer) {
-        super(clerk);
-        this.regularCustomer = regularCustomer;
-        this.discountCount = 0;
-        this.totalDiscount = 0.0;
+  @Override
+  public void add(Item item) {
+    super.add(item);
+    if (regularCustomer) {
+      double d = item.getDiscount();
+      if (d > 0.0) {
+        discountCount++;
+        discountAmount += d;
+      }
     }
-    
-    @Override
-    public void add(Item item) {
-        super.add(item);
-        if (regularCustomer && item.getDiscount() > 0.0) {
-            discountCount++;
-            totalDiscount += item.getDiscount();
-        }
+  }
+
+  @Override
+  public double getTotal() {
+    double full = super.getTotal();
+    if (!regularCustomer) {
+      return full;
     }
-    
-    @Override
-    public double getTotal() {
-        if (!regularCustomer) {
-            return super.getTotal();
-        }
-        return super.getTotal() - totalDiscount;
+    return full - discountAmount;
+  }
+
+  public int getDiscountCount() {
+    return regularCustomer ? discountCount : 0;
+  }
+
+  public double getDiscountAmount() {
+    return regularCustomer ? discountAmount : 0.0;
+  }
+
+  public double getDiscountPercent() {
+    if (!regularCustomer) {
+      return 0.0;
     }
-    
-    public int getDiscountCount() {
-        return discountCount;
+    double full = super.getTotal();
+    if (full <= 0.0) {
+      return 0.0;
     }
-    
-    public double getDiscountAmount() {
-        return totalDiscount;
-    }
-    
-    public double getDiscountPercent() {
-        double totalWithoutDiscount = super.getTotal();
-        if (totalWithoutDiscount == 0.0) {
-            return 0.0;
-        }
-        return 100 - (getTotal() * 100) / totalWithoutDiscount;
-    }
+    double paid = full - discountAmount;
+    return 100.0 - (paid * 100.0) / full;
+  }
 }
