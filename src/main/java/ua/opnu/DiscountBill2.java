e4f53ba9fe30970a6e072e5973eee05be8db47eb
Agehesha <romashugaev99@gmail.com>
2025-10-10 18:12:08 +0300

    Update DiscountBill2.java

--- src/main/java/ua/opnu/DiscountBill2.java
+++ src/main/java/ua/opnu/DiscountBill2.java
@@ -1,61 +1,76 @@
 package ua.opnu;
 
-
 import java.lang.reflect.Method;
 
-
 public class DiscountBill2 {
-private final GroceryBill bill;
-private final boolean regularCustomer;
-private int discountCount;
-private double discountAmount;
 
+  private final GroceryBill bill;
+  private final boolean regularCustomer;
 
-public DiscountBill2(Employee clerk, boolean regularCustomer) {
-this.bill = new GroceryBill(clerk);
-this.regularCustomer = regularCustomer;
-this.discountCount = 0;
-this.discountAmount = 0.0;
-}
+  private int discountCount;
+  private double discountAmount;
+  private double fullTotal;
+  private double payTotal;
 
+  public DiscountBill2(Employee clerk, boolean regularCustomer) {
+    this.bill = new GroceryBill(clerk);
+    this.regularCustomer = regularCustomer;
+  }
 
-public void add(Item i) {
-bill.add(i);
-if (regularCustomer) {
-double d = extractDiscount(i);
-if (d > 0.0) {
-discountCount++;
-discountAmount += d;
-}
-}
-}
+  public void add(Item item) {
+    if (item == null) {
+      return;
+    }
+    bill.add(item);
 
+    double price = item.getPrice();
+    double disc = regularCustomer ? extractDiscount(item) : 0.0;
 
-public double getTotal() {
-double full = bill.getTotal();
-if (!regularCustomer) return full;
-double total = full - discountAmount;
-return total < 0 ? 0 : total;
-}
+    fullTotal += price;
+    payTotal += price - Math.max(0.0, disc);
 
+    if (disc > 0.0) {
+      discountCount++;
+      discountAmount += disc;
+    }
+  }
 
-public int getDiscountCount() { return discountCount; }
-public double getDiscountAmount() { return regularCustomer ? discountAmount : 0.0; }
-public double getDiscountPercent() {
-if (!regularCustomer) return 0.0;
-double full = bill.getTotal();
-if (full <= 0.0) return 0.0;
-return 100.0 * (discountAmount / full);
-}
+  private double extractDiscount(Item item) {
+    try {
+      Method m = item.getClass().getMethod("getDiscount");
+      Object v = m.invoke(item);
+      if (v instanceof Number) {
+        double d = ((Number) v).doubleValue();
+        return d < 0 ? 0.0 : d;
+      }
+    } catch (Exception ignored) { }
+    return 0.0;
+  }
 
+  public double getTotal() {
+    return regularCustomer ? payTotal : fullTotal;
+  }
 
+  public int getDiscountCount() {
+    return regularCustomer ? discountCount : 0;
+  }
 
-private static double extractDiscount(Object item) {
-try {
-Method m = item.getClass().getMethod("getDiscount");
-Object val = m.invoke(item);
-if (val instanceof Number) return ((Number) val).doubleValue();
-} catch (Exception ignore) {}
-return 0.0;
-}
+  public double getDiscountAmount() {
+    return regularCustomer ? discountAmount : 0.0;
+  }
+
+  public double getDiscountPercent() {
+    if (!regularCustomer || fullTotal <= 0.0) {
+      return 0.0;
+    }
+    return 100.0 - (getTotal() * 100.0) / fullTotal;
+  }
+
+  public int getCount() {
+    return bill.getCount();
+  }
+
+  public Employee getClerk() {
+    return bill.getClerk();
+  }
 }
