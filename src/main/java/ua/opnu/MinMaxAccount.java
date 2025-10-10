2e6c9b0cb0fc7b76b1a4d993290d78c79605638e
Agehesha <romashugaev99@gmail.com>
2025-10-10 18:10:32 +0300

    Update MinMaxAccount.java

--- src/main/java/ua/opnu/MinMaxAccount.java
+++ src/main/java/ua/opnu/MinMaxAccount.java
@@ -1,37 +1,44 @@
 package ua.opnu;
 
-
 public class MinMaxAccount extends BankingAccount {
-private int min;
-private int max;
 
+  private int min;
+  private int max;
 
-public MinMaxAccount(Startup s) {
-super(s);
-int bal = getBalance();
-this.min = bal;
-this.max = bal;
-}
+  public MinMaxAccount(Startup s) {
+    super(s);
+    int bal = getBalance();
+    this.min = bal;
+    this.max = bal;
+  }
 
+  @Override
+  public void debit(int amount) {
+    super.debit(amount);
+    updateExtremes();
+  }
 
-public void debit(int amount) {
-super.debit(amount);
-touch(getBalance());
-}
+  @Override
+  public void credit(int amount) {
+    super.credit(amount);
+    updateExtremes();
+  }
 
+  private void updateExtremes() {
+    int bal = getBalance();
+    if (bal < min) {
+      min = bal;
+    }
+    if (bal > max) {
+      max = bal;
+    }
+  }
 
-public void credit(int amount) {
-super.credit(amount);
-touch(getBalance());
-}
+  public int getMin() {
+    return min;
+  }
 
-
-private void touch(int bal) {
-if (bal < min) min = bal;
-if (bal > max) max = bal;
-}
-
-
-public int getMin() { return min; }
-public int getMax() { return max; }
+  public int getMax() {
+    return max;
+  }
 }
