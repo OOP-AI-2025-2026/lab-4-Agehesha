c3316feedb73ec96bed5e1c2c0a8e633c4523559
Agehesha <romashugaev99@gmail.com>
2025-10-10 20:59:37 +0300

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -1,7 +1,9 @@
 package ua.opnu;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Lab 4: ready");
-    }
+public final class Main {
+  private Main() { }
+
+  public static void main(String[] args) {
+    System.out.println("lab-4t: ready");
+  }
 }
