d13b505e92170c9f142bac90b97a0b271994b555
Agehesha <romashugaev99@gmail.com>
2025-10-10 18:11:24 +0300

    Update Point3D.java

--- src/main/java/ua/opnu/Point3D.java
+++ src/main/java/ua/opnu/Point3D.java
@@ -1,57 +1,50 @@
 package ua.opnu;
 
-
 public class Point3D extends Point {
-private int z;
 
+  private int z;
 
-public Point3D() {
-super();
-this.z = 0;
-}
+  public Point3D() {
+    this(0, 0, 0);
+  }
 
+  public Point3D(int x, int y, int z) {
+    super(x, y);
+    this.z = z;
+  }
 
-public Point3D(int x, int y, int z) {
-super(x, y);
-this.z = z;
-}
+  public void setLocation(int x, int y, int z) {
+    super.setLocation(x, y);
+    this.z = z;
+  }
 
+  @Override
+  public void setLocation(int x, int y) {
+    super.setLocation(x, y);
+    this.z = 0;
+  }
 
-public void setLocation(int x, int y, int z) {
-super.setLocation(x, y);
-this.z = z;
-}
+  public int getZ() {
+    return z;
+  }
 
+  public double distance(Point3D p) {
+    long dx = (long) getX() - p.getX();
+    long dy = (long) getY() - p.getY();
+    long dz = (long) this.z - p.z;
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+  }
 
-@Override
-public void setLocation(int x, int y) {
-super.setLocation(x, y);
-this.z = 0;
-}
+  @Override
+  public double distanceFromOrigin() {
+    long lx = getX();
+    long ly = getY();
+    long lz = z;
+    return Math.sqrt(lx * lx + ly * ly + lz * lz);
+  }
 
-
-public int getZ() { return z; }
-
-
-public double distance(Point3D p) {
-long dx = (long) getX() - p.getX();
-long dy = (long) getY() - p.getY();
-long dz = (long) this.z - p.z;
-return Math.sqrt(dx*dx + dy*dy + dz*dz);
-}
-
-
-@Override
-public double distanceFromOrigin() {
-long x = getX();
-long y = getY();
-long z0 = this.z;
-return Math.sqrt(x*x + y*y + z0*z0);
-}
-
-
-@Override
-public String toString() {
-return "(" + getX() + ", " + getY() + ", " + z + ")";
-}
+  @Override
+  public String toString() {
+    return "(" + getX() + ", " + getY() + ", " + z + ")";
+  }
 }
