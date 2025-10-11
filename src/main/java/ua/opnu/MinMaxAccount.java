e36cf3daa7f42b8b48bb0e38ce8029c427be41a6
Agehesha <romashugaev99@gmail.com>
2025-10-11 19:28:15 +0300

    Update MinMaxAccount.java

--- src/main/java/ua/opnu/MinMaxAccount.java
+++ src/main/java/ua/opnu/MinMaxAccount.java
@@ -5,20 +5,15 @@ import ua.opnu.java.inheritance.account.Credit;
 import ua.opnu.java.inheritance.account.Debit;
 import ua.opnu.java.inheritance.account.Startup;
 
-/**
- * Счёт, который отслеживает минимальный и максимальный баланс,
- * наблюдавшийся после операций.
- */
+
 public class MinMaxAccount extends BankingAccount {
     private int minBalance;
     private int maxBalance;
 
-    /** Удобный конструктор под тесты: стартовый баланс целым числом. */
     public MinMaxAccount(int startBalance) {
         this(new Startup(startBalance));
     }
 
-    /** Базовый конструктор, если тесты/код передают Startup. */
     public MinMaxAccount(Startup startup) {
         super(startup);
         int bal = getBalance();
@@ -26,7 +21,6 @@ public class MinMaxAccount extends BankingAccount {
         this.maxBalance = bal;
     }
 
-    /** Перегрузка под тесты: пополнение целым числом. */
     public void deposit(int amount) {
         super.deposit(new Credit(amount));
         int bal = getBalance();
@@ -34,7 +28,6 @@ public class MinMaxAccount extends BankingAccount {
         if (bal > maxBalance) maxBalance = bal;
     }
 
-    /** Перегрузка под тесты: списание целым числом. */
     public void withdraw(int amount) {
         super.withdraw(new Debit(amount));
         int bal = getBalance();
