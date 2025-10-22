93942c24d7013d626626ac262848f1bacb5d7074
Agehesha <romashugaev99@gmail.com>
2025-10-23 02:50:40 +0300

    Update Point3D.java

--- src/main/java/ua/opnu/Point3D.java
+++ src/main/java/ua/opnu/Point3D.java
@@ -1,50 +1,46 @@
-package ua.opnu;
 
-import ua.opnu.java.inheritance.point.Point;
+package org.example.lab4.geom;
+
+import org.example.lab4.docsmodel.Point;
 
 
 public class Point3D extends Point {
     private int z;
 
+    public Point3D() { this(0, 0, 0); }
     public Point3D(int x, int y, int z) {
         super(x, y);
         this.z = z;
     }
 
-    public int getZ() { return z; }
-    public void setZ(int z) { this.z = z; }
 
-    public void move(int dx, int dy, int dz) {
-        super.move(dx, dy); 
-        this.z += dz;
+    public void setLocation(int x, int y, int z) {
+        super.setLocation(x, y);
+        this.z = z;
     }
+
+
+    @Override
+    public void setLocation(int x, int y) {
+        super.setLocation(x, y);
+        this.z = 0;
+    }
+
 
     public double distance(Point3D p) {
-        long dx = (long) getX() - p.getX();
-        long dy = (long) getY() - p.getY();
-        long dz = (long) this.z   - p.z;
-        return Math.sqrt(dx*dx + dy*dy + dz*dz);
+        int dx = getX() - p.getX();
+        int dy = getY() - p.getY();
+        int dz = this.z - p.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Point3D)) return false;
-        Point3D other = (Point3D) o;
-        return getX() == other.getX() && getY() == other.getY() && z == other.z;
-    }
 
     @Override
-    public int hashCode() {
-        int r = 17;
-        r = 31 * r + getX();
-        r = 31 * r + getY();
-        r = 31 * r + z;
-        return r;
+    public double distanceFromOrigin() {
+        return Math.sqrt(getX() * getX() + getY() * getY() + z * z);
     }
 
-    @Override
-    public String toString() {
-        return "(" + getX() + ", " + getY() + ", " + z + ")";
-    }
+    public int getZ() { return z; }
+
+    @Override public String toString() { return "(" + getX() + ", " + getY() + ", " + z + ")"; }
 }
