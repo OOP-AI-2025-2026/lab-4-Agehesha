41efa4f2d4ddab093053dbd162240cfb233fc952
Agehesha <romashugaev99@gmail.com>
2025-10-23 11:29:53 +0300

    Create Startup.java

---
+++ src/main/java/ua/opnu/java/inheritance/account/Startup.java
@@ -0,0 +1,13 @@
+package ua.opnu.java.inheritance.account;
+
+public final class Startup {
+    private final int initialCents;
+
+    public Startup(int initialCents) {
+        this.initialCents = initialCents;
+    }
+
+    public int getInitialCents() {
+        return initialCents;
+    }
+}
